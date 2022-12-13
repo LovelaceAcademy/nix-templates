4e2e856adba3b764ddedcc0efb011955188ffe3a
Walker <walker@praiseweb.com.br>
2022-12-13 12:44:30 -0300

    ci: add haskell-nix template test

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
+TMPL=$1
 PWD=$(pwd)
 TEST_DIR=.test-project
 TMP_DIR=/tmp/nix-templates
+
+# clean up any previous data
 set +ex
 rm -Rf $TMP_DIR
 rm -Rf $TEST_DIR
 set -ex
-nix flake check --show-trace
+
+# test the template
 cp -Rf . $TMP_DIR
-FILE=$TMP_DIR/nix/template/flake.nix
+FILE=$TMP_DIR/${TMPL}/flake.nix
 sed -i "s|github:LovelaceAcademy/nix-templates|path:$PWD|g" $FILE
 mkdir $TEST_DIR
 (
 	cd $TEST_DIR
 	git init
-	nix flake init -t $TMP_DIR
+	nix flake init -t "${TMP_DIR}#${TMPL}"
 	nix flake check --show-trace
 )
