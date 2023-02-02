64f6fbec1429af27157029f767f7c30ff11f9aed
Walker <walker@praiseweb.com.br>
2023-02-02 18:14:40 -0300

    feat(ctl-full): add webpack, purs-nix, serve, dev, bundle

--- ctl-full/flake.nix
+++ ctl-full/flake.nix
@@ -41,6 +41,43 @@
               # foreign.Main.node_modules = [];
             };
           ps-command = ps.command { };
+          purs-watch = pkgs.writeShellApplication {
+            name = "purs-watch";
+            runtimeInputs = with pkgs; [ entr ps-command ];
+            text = "find src | entr -s 'echo building && purs-nix compile'";
+          };
+          webpack = pkgs.writeShellApplication {
+            name = "webpack";
+            runtimeInputs = with pkgs; [ nodejs ];
+            text = ''npx webpack "$@"'';
+          }; 
+          serve = pkgs.writeShellApplication {
+            name = "serve";
+            runtimeInputs = with pkgs; [ webpack ];
+            text = ''BROWSER_RUNTIME=1 webpack serve --progress --open "$@"'';
+          };
+          dev = pkgs.writeShellApplication {
+            name = "dev";
+            runtimeInputs = with pkgs; [
+              concurrently
+              purs-watch
+              serve
+            ];
+            text = ''
+              concurrently\
+                --handle-input\
+                --restart-tries 10\
+                purs-watch\
+                serve\
+            '';
+          };
+          bundle = pkgs.writeShellApplication {
+            name = "bundle";
+            runtimeInputs = with pkgs; [
+              webpack
+            ];
+            text = ''BROWSER_RUNTIME=1 webpack --mode=production "$@"'';
+          };
         in
         {
           packages.default = ps.output { };
@@ -54,7 +91,19 @@
                     easy-ps.purescript-language-server
                     purs
                     ps-command
+                    purs-watch
+                    webpack
+                    serve
+                    dev
+                    bundle
                   ];
+                shellHook = ''
+                  alias log_='printf "\033[1;32m%s\033[0m\n" "$@"'
+                  alias info_='printf "\033[1;34m[INFO] %s\033[0m\n" "$@"'
+                  log_ "Welcome to ctl-full shell."
+                  info_ "Available commands: webpack, purs-nix, serve, dev, bundle."
+                  info_ "testnet-magic for preprod is 1"
+                '';
               };
         });
 
