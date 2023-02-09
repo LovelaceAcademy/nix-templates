cd127ce371a3a733c66666a0d1868676ad0d9950
Walker <walker@praiseweb.com.br>
2023-02-09 20:35:53 -0300

    feat(plutus): add a minimal typed plutus example, add plutus-apps

--- plutus/src/hello.hs
+++ plutus/src/hello.hs
@@ -4,20 +4,29 @@
 module Main (main) where
 
 import Prelude (IO)
-import PlutusTx.Prelude (($), BuiltinData, Maybe (Nothing))
+import PlutusTx.Prelude (($), (==), Maybe (Nothing), Bool (True, False), Integer)
 import Cardano.Api.Shelley (PlutusScript (PlutusScriptSerialised), PlutusScriptV2)
 import Cardano.Api.SerialiseTextEnvelope (textEnvelopeToJSON)
 import qualified Codec.Serialise as CS
 import qualified Data.ByteString.Lazy as DBL
 import qualified Data.ByteString.Short as DBS
-import PlutusTx (compile)
-import Plutus.V2.Ledger.Api (Validator, mkValidatorScript)
+import PlutusTx (compile, unstableMakeIsData)
+import Plutus.V2.Ledger.Api (Validator, ScriptContext, mkValidatorScript)
+import Plutus.Script.Utils.Typed (IsScriptContext (mkUntypedValidator))
 
-validator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
-validator datum redeemer context = ()
+newtype MyDatum = MyDatum Integer
+unstableMakeIsData ''MyDatum
+newtype MyRedeemer = MyRedeemer Integer
+unstableMakeIsData ''MyRedeemer
+
+validator :: MyDatum -> MyRedeemer -> ScriptContext -> Bool
+validator _ (MyRedeemer n) _ | n == 42 = True 
+validator _ _ _ = False 
 
 validator' :: Validator
-validator' = mkValidatorScript $$(compile [||validator||])
+validator' = mkValidatorScript $$(compile [|| wrap ||])
+  where
+    wrap = mkUntypedValidator validator
 
 serialise :: Validator -> PlutusScript PlutusScriptV2
 serialise val = PlutusScriptSerialised $ DBS.toShort $ DBL.toStrict $ CS.serialise $ val
