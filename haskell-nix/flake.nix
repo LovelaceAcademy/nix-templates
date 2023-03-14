10e3f2b882d848c5762936ef307b74a4e0789da5
Walker <walker@praiseweb.com.br>
2023-03-13 23:57:26 -0300

    fix: remove broken plutonomicon cache

--- haskell-nix/flake.nix
+++ haskell-nix/flake.nix
@@ -35,14 +35,12 @@
       "https://cache.iog.io"
       "https://cache.zw3rk.com"
       "https://cache.nixos.org"
-      "https://public-plutonomicon.cachix.org"
     ];
     extra-trusted-public-keys = [
       "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="
     ];
     allow-import-from-derivation = "true";
   };
