9eb852a86ac0b8454ec1652dc726af594a76147c
Walker <walker@praiseweb.com.br>
2023-03-13 23:43:43 -0300

    fix: remove broken iohk cache

--- ctl/flake.nix
+++ ctl/flake.nix
@@ -66,7 +66,6 @@
     extra-substituters = [
       "https://klarkc.cachix.org?priority=99"
       "https://cache.iog.io"
-      "https://iohk.cachix.org"
       "https://cache.zw3rk.com"
       "https://cache.nixos.org"
       "https://public-plutonomicon.cachix.org"
@@ -74,7 +73,6 @@
     extra-trusted-public-keys = [
       "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="
