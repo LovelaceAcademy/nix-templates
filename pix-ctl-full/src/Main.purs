6dacef642c3c4a4ac5a16c0186841d31e8aa92e8
Walker <walker@praiseweb.com.br>
2023-03-16 16:57:06 -0300

    fix(pix-ctl-full): fix example

--- pix-ctl-full/src/Main.purs
+++ pix-ctl-full/src/Main.purs
@@ -1,16 +1,25 @@
 module Main (contract, main) where
 
 import Contract.Prelude
-
+  ( ($)
+  , Effect
+  , Unit
+  , void
+  , bind
+  , liftEither
+  , discard
+  , show
+  )
 import Contract.Address (ownPaymentPubKeyHash)
 import Contract.Log (logInfo')
 import Contract.Monad (Contract, launchAff_, runContract)
 import Contract.Config (testnetEternlConfig)
+import Scripts (validator)
 
 contract :: Contract Unit
 contract = do
-  validator <- liftEither validator
-  logInfo' validator
+  validator' <- liftEither validator
+  logInfo' $ show validator'
   pk <- ownPaymentPubKeyHash
   logInfo' $ show pk
 
