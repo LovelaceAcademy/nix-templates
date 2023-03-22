7ec1890908ee3ed7cacbec279f6d4a3b64220362
Walker <walker@praiseweb.com.br>
2023-03-22 13:25:32 -0300

    feat: add herculis ci cache

--- hix/flake.nix
+++ hix/flake.nix
@@ -35,12 +35,14 @@
       "https://cache.iog.io"
       "https://cache.zw3rk.com"
       "https://cache.nixos.org"
+      "https://hercules-ci.cachix.org"
     ];
     extra-trusted-public-keys = [
       "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
     ];
     allow-import-from-derivation = "true";
   };
