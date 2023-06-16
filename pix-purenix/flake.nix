2baa9487cdfc93d10b24576f93a2032e7572c7be
klarkc <walkerleite490@gmail.com>
2023-06-16 13:16:29 -0300

    feat(pix-purenix): remove herculis cache, we're not using it

--- pix-purenix/flake.nix
+++ pix-purenix/flake.nix
@@ -81,12 +81,10 @@
     extra-substituters = [
       "https://klarkc.cachix.org?priority=99"
       "https://cache.nixos.org"
-      "https://hercules-ci.cachix.org"
     ];
     extra-trusted-public-keys = [
       "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
     ];
   };
 }
