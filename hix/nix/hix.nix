e9828c369d6f0862015c80a0536ccf54acc331fa
klarkc <walkerleite490@gmail.com>
2023-09-15 17:30:18 -0300

    feat(hix): enable lsp and lint

--- hix/nix/hix.nix
+++ hix/nix/hix.nix
@@ -15,6 +15,6 @@
 
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
-  # shell.tools.hlint = "latest";
-  # shell.tools.haskell-language-server = "latest";
+  shell.tools.hlint = "latest";
+  shell.tools.haskell-language-server = "latest";
 }
