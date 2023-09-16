41a45256d65f42c1ec2a6e4ee649ab2a5585996a
klarkc <walkerleite490@gmail.com>
2023-09-16 17:21:25 -0300

    feat(hor,hor-plutus): add hor proper cache

--- hor-plutus/flake.nix
+++ hor-plutus/flake.nix
@@ -69,13 +69,11 @@
     # but remove it here if you do not want it to.
     extra-substituters = [
       "https://klarkc.cachix.org?priority=99"
-      "https://cache.iog.io"
-      "https://cache.zw3rk.com"
+      "https://horizon.cachix.org"
     ];
     extra-trusted-public-keys = [
       "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
+      "horizon.cachix.org-1:MeEEDRhRZTgv/FFGCv3479/dmJDfJ82G6kfUDxMSAw0="
     ];
   };
 }
