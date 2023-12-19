1da131e5236b3088755c38bb725f4e0e083e9078
klarkc <walkerleite490@gmail.com>
2023-12-18 22:45:23 -0300

    fix(pix-ctl-full): readd dev and bundle cmds

--- pix-ctl-full/webpack.config.js
+++ pix-ctl-full/webpack.config.js
@@ -3,9 +3,42 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
+const isBrowser = !!process.env.BROWSER_RUNTIME;
+
+const plugins = [
+  new webpack.DefinePlugin({
+    BROWSER_RUNTIME: isBrowser,
+  }),
+  new webpack.LoaderOptionsPlugin({
+    debug: true,
+  }),
+  new HtmlWebpackPlugin({
+    title: "pix-ctl-full",
+    template: "./index.html",
+    inject: false, // See stackoverflow.com/a/38292765/3067181
+  }),
+  // ContextReplacementPlugin is used just to suppress a webpack warning:
+  // "Critical dependency: the request of a dependency is an expression"
+  // See https://stackoverflow.com/a/59235546/17365145
+  new webpack.ContextReplacementPlugin(/cardano-serialization-lib-browser/),
+  new webpack.ContextReplacementPlugin(/cardano-serialization-lib-nodejs/),
+];
+
+if (isBrowser) {
+  // Provide top-level `Buffer`
+  plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  );
+  // Provide NodeJS polyfills
+  plugins.push(new NodePolyfillPlugin());
+}
+
 module.exports = {
   mode: "development",
-
+  target:  isBrowser ? "web" : "node18",
+  node: isBrowser ? {} : { __dirname: true },
   experiments: {
     asyncWebAssembly: false,
     layers: false,
@@ -37,26 +70,24 @@ module.exports = {
     aggregateTimeout: 1000,
   },
 
+  // Preserves console.log calls in NodeJS
+  // https://stackoverflow.com/a/71024096/17365145
+  optimization:  isBrowser? {}: { minimize: false },
+
   // we can add more entrypoints as needed
   entry: "./index.js",
 
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg|gif)$/i,
-        type: "asset",
-      },
-    ],
+    library: {
+      type: "module",
+    },
   },
 
   resolve: {
     extensions: [".js"],
-    fallback: {
+    fallback: isBrowser? {
       buffer: require.resolve("buffer/"),
       http: false,
       url: false,
@@ -69,26 +100,10 @@ module.exports = {
       os: false,
       path: false,
       fs: false,
-    },
+      readline: false,
+      child_process: false,
+    }: {},
   },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      BROWSER_RUNTIME: !!process.env.BROWSER_RUNTIME,
-    }),
-    new NodePolyfillPlugin(),
-    new webpack.LoaderOptionsPlugin({
-      debug: true,
-    }),
-    new HtmlWebpackPlugin({
-      title: "pix-ctl-full",
-      template: "./index.html",
-      inject: false, // See stackoverflow.com/a/38292765/3067181
-    }),
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-    }),
-    new webpack.ContextReplacementPlugin(/cardano-serialization-lib-browser/),
-    new webpack.ContextReplacementPlugin(/cardano-serialization-lib-nodejs/),
-  ],
+  plugins,
 };
