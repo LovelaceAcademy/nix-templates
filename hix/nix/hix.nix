8337d49ada534e998dc3951cfffd4503eb3935f5
klarkc <walkerleite490@gmail.com>
2023-12-22 14:47:31 -0300

    feat(hix): bump GHC

--- hix/nix/hix.nix
+++ hix/nix/hix.nix
@@ -3,7 +3,7 @@
   
   # We use the latest supported and cached version
   # from github:input-output-hk/haskell.nix
-  compiler-nix-name = "ghc8107";
+  compiler-nix-name = "ghc92";
 
   # Enable for cross-platform build
   # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
@@ -15,8 +15,7 @@
 
   # Tools to include in the development shell
   shell.tools.cabal = "latest";
-  # FIXME hlint not building with ghc8107
+  shell.tools.haskell-language-server = "latest";
+  # The language server already includes a formatter
   # shell.tools.hlint = "latest";
-  # FIXME haskell-language-server wih ghc8107
-  # shell.tools.haskell-language-server = "2.0.0.0";
 }
